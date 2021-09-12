781155e22f04e60cc56642923b9fdcff11be7c18
Augusto Esquen Espinoza <augusto.esquen2@gmail.com>
2021-09-11 22:35:41 -0500

    Creando Initializr

---
+++ src/main/java/com/entelgyapi/EntelgyApiApplication.java
@@ -0,0 +1,13 @@
+package com.entelgyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntelgyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntelgyApiApplication.class, args);
+	}
+
+}
